125b101874641101df8d23b2cbb129b595140353
hcg <673035915@qq.com>
2020-04-27 14:23:01 +0000

    Move user model to models folder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('account')->unique();
             $table->string('password');
+            $table->string('avatar')->default('/storage/images/default.jpg');
             $table->rememberToken();
             $table->timestamps();
         });
