4cd40b9400881a06276d3cd8d83a789596f66648
hcg <673035915@qq.com>
2020-04-27 13:01:18 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,22 @@
 <html>
   <head>
     <title>@yield('title', '福大校友录') - 毕业设计</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">福大校友录</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1 style="color: red">欢迎！</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">校友录</a> 的后台管理页面。
+    </p>
+    <p style="color: purple">
+      从创建一个用户开始...
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">创建</a>
+    </p>
+  </div>
 @stop
