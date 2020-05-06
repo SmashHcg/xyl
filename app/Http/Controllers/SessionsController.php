00f7e0948ca3d998d2ac6c2f6040290c514b53b4
hcg <673035915@qq.com>
2020-05-06 11:01:24 +0000

    登录视图修改

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -36,7 +36,7 @@ class SessionsController extends Controller
            $fallback = route('users.show', Auth::user());
            return redirect()->route('users.show', [Auth::user()]);
        } else {
-           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           session()->flash('danger', '很抱歉，您的学号和密码不匹配');
            return redirect()->back()->withInput();
        }
     }
